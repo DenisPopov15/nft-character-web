5a3a852715b0719a72a648228e69f322e3c6bdb6
Denis <denis@slatestudio.com>
2023-05-13 16:25:49 +0300

    Update characteristic page route

--- src/paths.js
+++ src/paths.js
@@ -5,7 +5,5 @@ export var APP_PATHS
   APP_PATHS['characterPersona'] = '/character-persona/'
   APP_PATHS['characterStory'] = '/character-story/'
   APP_PATHS['chat'] = '/chat/'
-  APP_PATHS[
-    'nftCharacteristics'
-  ] = `${APP_PATHS.nftList}/:nftId/characteristics`
+  APP_PATHS['nftCharacteristics'] = `${APP_PATHS.nftList}:nftId/personallity/`
 })(APP_PATHS || (APP_PATHS = {}))
